c73548f878a086f6f32974ed5b5778b56fba939f
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-07-24 22:20:20 +0300

    added basic cli app structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+#[derive(Debug, Subcommand)]
+enum Commands {
+    List,
+    Add,
+    Select,
+    Check,
+    Uncheck,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    match args.command {
+        Commands::List => todo!("add list subcommand"),
+        Commands::Add => todo!("add add subcommand"),
+        Commands::Select => todo!("add select subcommand"),
+        Commands::Check => todo!("add check subcommand"),
+        Commands::Uncheck => todo!("adduncheck subcommand"),
+    }
+    return Ok(());
 }
