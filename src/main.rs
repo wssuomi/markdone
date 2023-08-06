96c0711ffdc8ac031d4e0067430972d22951ac65
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-08-07 01:18:02 +0300

    feat: add create subcommand to create todo file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use anyhow::Result;
 use clap::{Parser, Subcommand};
+use std::{
+    fs::File,
+    io::{BufRead, BufReader, Write},
+    path::PathBuf,
+};
 
 #[derive(Debug, Parser)]
 struct Cli {
@@ -21,7 +26,14 @@ fn main() -> Result<()> {
     match args.command {
         Commands::Add => todo!("add add subcommand"),
         Commands::Check => todo!("add check subcommand"),
-        Commands::Create => todo!("add create subcommand"),
+        Commands::Create => {
+            let path: PathBuf = PathBuf::from("markdone.md");
+            let mut file = File::create(&path)?;
+            file.write_all(
+                b"### SELECTED\n\n---\n\n### INCOMPLETE\n\n---\n\n### COMPLETE\n\n---\n",
+            )?;
+            println!("successfully created `{:?}`", &path);
+        }
         Commands::List => todo!("add list subcommand"),
         Commands::Select => todo!("add select subcommand"),
         Commands::Uncheck => todo!("add uncheck subcommand"),
