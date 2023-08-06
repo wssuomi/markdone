724cf86fcba6a071f65f2bb9a22d4e1c5cfc70c2
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-08-07 00:15:57 +0300

    style: sorted subcommands

--- src/main.rs
+++ src/main.rs
@@ -8,23 +8,23 @@ struct Cli {
 }
 #[derive(Debug, Subcommand)]
 enum Commands {
-    List,
     Add,
-    Select,
     Check,
-    Uncheck,
     Create,
+    List,
+    Select,
+    Uncheck,
 }
 
 fn main() -> Result<()> {
     let args = Cli::parse();
     match args.command {
-        Commands::List => todo!("add list subcommand"),
         Commands::Add => todo!("add add subcommand"),
-        Commands::Select => todo!("add select subcommand"),
         Commands::Check => todo!("add check subcommand"),
-        Commands::Uncheck => todo!("add uncheck subcommand"),
         Commands::Create => todo!("add create subcommand"),
+        Commands::List => todo!("add list subcommand"),
+        Commands::Select => todo!("add select subcommand"),
+        Commands::Uncheck => todo!("add uncheck subcommand"),
     }
     return Ok(());
 }
