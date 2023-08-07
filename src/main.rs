671cfebf60051aebd24b0fb047581919dfc5e947
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-08-07 13:23:51 +0300

    feat: add add subcommand to add new tasks to file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,21 @@
 use anyhow::{bail, Context, Result};
 use clap::{Parser, Subcommand};
-use std::{fs::File, io::Write, path::PathBuf};
+use std::{
+    cmp::Ordering,
+    fs::{File, OpenOptions},
+    io::{BufRead, BufReader, Seek, SeekFrom, Write},
+    path::PathBuf,
+};
 
 #[derive(Debug, Parser)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
 }
+
 #[derive(Debug, Subcommand)]
 enum Commands {
-    Add,
+    Add { task: String },
     Check,
     Create,
     List,
@@ -17,10 +23,52 @@ enum Commands {
     Uncheck,
 }
 
+fn get_lines(path: &PathBuf) -> Result<Vec<String>> {
+    return Ok(BufReader::new(File::open(path)?)
+        .lines()
+        .collect::<Result<_, _>>()?);
+}
+
+fn get_section_start(lines: &Vec<String>, section: &str) -> Result<usize> {
+    let section_start: usize = lines
+        .iter()
+        .position(|value| value == &format!("### {}", section))
+        .with_context(|| format!("could not find `{:?}` section start", section))?;
+    return Ok(section_start);
+}
+
+fn get_section_end(lines: &Vec<String>, section_start: usize) -> Result<usize> {
+    let section_end = section_start
+        + lines[section_start..lines.len()]
+            .iter()
+            .position(|x| x == "---")
+            .with_context(|| format!("could not find `{:?}` section end", lines[section_start]))?;
+    return Ok(section_end);
+}
+
 fn main() -> Result<()> {
     let args = Cli::parse();
     match args.command {
-        Commands::Add => todo!("add add subcommand"),
+        Commands::Add { task } => {
+            let path: PathBuf = PathBuf::from("markdone.md");
+            let mut lines: Vec<String> = get_lines(&path)
+                .with_context(|| format!("could not read lines from file `{:?}`", path))?;
+            let section_start = get_section_start(&lines, "INCOMPLETE")?;
+            let section_end = get_section_end(&lines, section_start)?;
+            match (section_end - section_start).cmp(&2) {
+                Ordering::Equal => {
+                    lines.insert(section_end, String::from(""));
+                }
+                _ => (),
+            }
+            lines.insert(section_start + 2, format!("- [ ] {}", task));
+            let mut file = OpenOptions::new().write(true).open(path)?;
+            file.seek(SeekFrom::Start(0))?;
+            for line in lines {
+                writeln!(file, "{}", line)?;
+            }
+            println!("successfully added task `{:?}`", task);
+        }
         Commands::Check => todo!("add check subcommand"),
         Commands::Create => {
             let path: PathBuf = PathBuf::from("markdone.md");
