a4a5e572826263042246426b52290bb5bba3cddc
Walsu <111709514+wssuomi@users.noreply.github.com>
2023-08-16 21:33:05 +0300

    feat: add check command to mark task complete

--- src/main.rs
+++ src/main.rs
@@ -16,7 +16,7 @@ struct Cli {
 #[derive(Debug, Subcommand)]
 enum Commands {
     Add { task: String },
-    Check,
+    Check { id: usize },
     Create,
     List,
     Select { id: usize },
@@ -106,7 +106,79 @@ fn main() -> Result<()> {
                 task, task_count
             );
         }
-        Commands::Check => todo!("add check subcommand"),
+        Commands::Check { id } => {
+            let path: PathBuf = PathBuf::from("markdone.md");
+            let mut lines: Vec<String> = get_lines(&path)
+                .with_context(|| format!("could not read lines from file `{:?}`", path))?;
+            let complete_section_start = get_section_start(&lines, "COMPLETE")?;
+            let complete_section_end = get_section_end(&lines, complete_section_start)?;
+            let (task_idx, task_count): (usize, usize) = match get_task_idx_in_section(
+                &lines[complete_section_start..complete_section_end],
+                id,
+            ) {
+                Some(_) => {
+                    bail!("task with id `{:?}` is already checked", id);
+                }
+                None => {
+                    let selected_section_start = get_section_start(&lines, "SELECTED")?;
+                    let selected_section_end = get_section_end(&lines, selected_section_start)?;
+
+                    match get_task_idx_in_section(
+                        &lines[selected_section_start..selected_section_end],
+                        id,
+                    ) {
+                        Some(idx) => (
+                            idx + selected_section_start,
+                            get_task_count_in_section(
+                                &lines[selected_section_start..selected_section_end],
+                            ),
+                        ),
+                        None => {
+                            let incomplete_section_start = get_section_start(&lines, "INCOMPLETE")?;
+                            let incomplete_section_end =
+                                get_section_end(&lines, incomplete_section_start)?;
+                            match get_task_idx_in_section(
+                                &lines[incomplete_section_start..incomplete_section_end],
+                                id,
+                            ) {
+                                Some(idx) => (
+                                    idx + incomplete_section_start,
+                                    get_task_count_in_section(
+                                        &lines[incomplete_section_start..incomplete_section_end],
+                                    ),
+                                ),
+                                None => {
+                                    bail!("could not find task with id `{:?}`", id);
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            let task = lines.remove(task_idx);
+            let mut chars: Vec<char> = task.chars().collect();
+            chars[3] = 'x';
+            let task: String = chars.into_iter().collect();
+            if task_count == 1 {
+                lines.remove(task_idx);
+            }
+            let complete_section_start = get_section_start(&lines, "COMPLETE")?;
+            let complete_section_end = get_section_end(&lines, complete_section_start)?;
+            match (complete_section_end - complete_section_start).cmp(&2) {
+                Ordering::Equal => {
+                    lines.insert(complete_section_end, String::from(""));
+                }
+                _ => (),
+            };
+            lines.insert(complete_section_start + 2, task);
+            let mut file = OpenOptions::new().write(true).open(path)?;
+            file.seek(SeekFrom::Start(0))?;
+
+            for line in lines {
+                writeln!(file, "{}", line)?;
+            }
+            println!("successfully checked task with id `{:?}`", id);
+        }
         Commands::Create => {
             let path: PathBuf = PathBuf::from("markdone.md");
             if path.exists() {
